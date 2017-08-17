c7611c48d41c650e7f67250c55e4481daa8f1eea
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-17 15:57:53 -0400

    prettified

--- ReversePolishCalc.cs
+++ ReversePolishCalc.cs
@@ -1,26 +1,22 @@
 using System;
 using System.Collections.Generic;
 
-public class ReversePolishCalc {
-
-    // You'll need a variable here to keep track of the top of the stack
-
+public class ReversePolishCalc
+{
     // The array of the input string split up
     private String[] tokens;
 
     // The stack
     private Stack<string> stack = new Stack<string>();
 
-    public double Calculate(String input) {
+    public double Calculate(String input)
+    {
 
         // 1. Use the String split method to split the string into tokens at the commas
         tokens = input.Split(',');
-        Console.WriteLine(tokens);
-
         
-
         // 3. write the algorithm
-        for(int i = 0; i < tokens.Length; ++i) 
+        for (int i = 0; i < tokens.Length; ++i)
         {
             if (tokens[i] == "+")
             {
